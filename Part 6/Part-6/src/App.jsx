e908951ab977e653bd91935a45f12ef83fdc760b
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-06-19 11:23:28 +0300

    before the exercise

--- Part 6/Part-6/src/App.jsx
+++ Part 6/Part-6/src/App.jsx
@@ -1,27 +1,16 @@
-import { create } from 'zustand'
+import Controls from "./Controls";
+import Display from "./Display";
 
-const userCounterStore = create(set => ({
-  counter: 0,
-  increment: () => set(state => ({ counter: state.counter + 1 })),
-  decrement: () => set(state => ({ counter: state.counter - 1 })),
-  zero: () => set(() => ({counter: 0 }))
-}))
+
 
 const App = () => {
-  const counter = userCounterStore(state => state.counter)
-  const increment = userCounterStore(state => state.increment)
-  const decrement = userCounterStore(state => state.decrement)
-  const zero = userCounterStore(state => state.zero)
+  
 
   return (
-    <div>
-      <div>{counter}</div>
-      <div>
-        <button onClick={increment}>plus</button>
-        <button onClick={decrement}>minus</button>
-        <button onClick={zero}>zero</button>
-      </div>
-    </div>
+   <div>
+    <Display/>
+    <Controls/>
+  </div>
   )
 }
 
