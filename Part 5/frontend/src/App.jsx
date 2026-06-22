0337f3b200b900c5d9cd0394ee48889bff54d232
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-06-22 12:27:58 +0300

    Uncontrolled form

--- Part 5/frontend/src/App.jsx
+++ Part 5/frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import { useNotes } from "./store"
+import { useNoteActions, useNotes } from "./store"
 
 const App = () => {
   const notes = useNotes()
+  const { add } = useNoteActions()
+
+  const generatedId = () => Number((Math.random()* 1000000).toFixed(0))
+
+  const addNote = (e) => {
+    e.preventDefault()
+    const content = e.target.note.value
+    add({id: generatedId(), content, important: false})
+    e.target.reset()
+  }
 
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note"/>
+        <button type="submit">add</button>
+      </form>
       <ul>
         {notes.map(note => (
           <li>
