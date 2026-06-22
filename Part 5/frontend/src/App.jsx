52536bb806029c5be43591d16db82cec2abdd806
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-06-22 10:59:25 +0300

    Zustand notes

--- Part 5/frontend/src/App.jsx
+++ Part 5/frontend/src/App.jsx
@@ -1,137 +1,18 @@
-import { useState, useEffect } from 'react'
-import Footer from './components/Footer'
-import Note from './components/Note'
-import Notification from './components/Notification'
-import noteService from './services/notes'
-import Togglable from './components/Togglable'
-import NoteForm from './components/NoteForm'
-import { AppBar, Button, Container, Toolbar } from '@mui/material'
-import {
-  BrowserRouter as Router,
-  Routes, Route, Link,
-  useMatch
-} from 'react-router-dom'
-import NoteList from './components/NoteList'
-import Home from './components/Home'
-import { Alert } from '@mui/material'
+import { useNotes } from "./store"
 
-
-
-
-
-
-
-
-import styled from 'styled-components'
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [notification, setNotification] = useState(null)
-
-  useEffect(() => {
-    noteService.getAll().then(initialNotes => {
-      setNotes(initialNotes)
-    })
-  }, [])
-
-  const deleteNote = (id) => {
-    noteService.remove(id).then(() => {
-      setNotes(notes.filter(n => n.id !== id))
-    })
-  }
-
-  const toggleImportanceOf = id => {
-      const note = notes.find(n => n.id === id)
-      const changedNote = { ...note, important: !note.important }
-  
-      noteService
-        .update(id, changedNote)
-        .then(returnedNote => {
-          setNotes(notes.map(note => (note.id !== id ? note : returnedNote)))
-        })
-        .catch(() => {
-          setNotification({
-            text: `Note '${note.content}' was already removed from server`,
-            type: 'error'
-          })
-          setTimeout(() => {
-            setNotification(null)
-          }, 5000)
-          setNotes(notes.filter(n => n.id !== id))
-        })
-    }
-
-  const addNote = (noteObject) => {
-    noteService.create(noteObject).then(returnedNote => {
-      setNotes(notes.concat(returnedNote))
-      setNotification({ 
-        text: `Note '${returnedNote.content}' added!`, 
-        type:'success'
-      })
-
-      setTimeout(() => {
-        setNotification(null)
-      }, 5000)
-    })
-  }
-
-
-
-  const match = useMatch('/notes/:id')
-  const note = match ? notes.find(note => note.id === match.params.id)
-  : null
-
-  const style = { '&:hover': { bgcolor: 'rgba(255, 255, 255, 0.3)' } }
-
-  const Page = styled.div`
-    padding: 1em;
-    background: papayawhip;
-    `
-  const Navigation = styled.div`
-    background: BurlyWood;
-    padding: 1em;
-  `
-  const Footer = styled.div`
-    background: Chocolate;
-    padding: 1em;
-    margin-top: 1em;
-  `
-
+  const notes = useNotes()
 
   return (
-
-    <Page>
-        <AppBar position='static'>
-          <Navigation>
-            <Button color='inherit' component={Link}  to="/" sx={style}>home</Button>
-            <Button color='inherit' component={Link} to="/notes" sx={style}>notes</Button>
-            <Button color='inherit' component={Link} to="/create" sx={style}>new note</Button>
-          </Navigation>
-        </AppBar>
-
-        <Notification notification={notification}/>
-
-        <Routes>
-          <Route path="/notes/:id" element ={
-            <Note notes={notes} toggleImportanceOf={toggleImportanceOf} note={note} deleteNote={deleteNote}/>
-          }/>
-          <Route path="/notes" element={
-            <NoteList
-            notes={notes} 
-            toggleImportanceOf={toggleImportanceOf}
-            setNotification={setNotification}
-            deleteNote={deleteNote}
-            />
-          }/>
-          <Route path="/create" element={     
-            <NoteForm createNote={addNote}/>
-          }/>
-          <Route path="/" element={<Home/>} />
-      </Routes>
-      <Footer>
-         Note app, Department of Computer Science, University of Helsinki 2026
-      </Footer>
-    </Page>
-    
+    <div>
+      <ul>
+        {notes.map(note => (
+          <li>
+            {note.important ? <strong>{note.content}</strong> : note.content}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
