ee42f4438ce837fcd292702a16041d8393e2c99a
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-07-09 16:23:30 +0300

    Synchronizing data to the server using TanStack Query

--- Part 6/notes-app/src/requests.js
+++ Part 6/notes-app/src/requests.js
@@ -6,4 +6,37 @@ export const getNotes = async () => {
     throw new Error('Failed to fetch notes')
   }
   return await response.json()
+
+}
+
+export const createNote = async (newNote) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(newNote)
+  }
+
+  const response = await fetch(baseUrl, options)
+
+  if(!response.ok){
+    throw new Error('Failed to create a note')
+  }
+
+  return await response.json()
+}
+
+export const updateNote = async (updatedNote) => {
+  const options = {
+    method: 'PUT',
+    headers: { 'Content-Type' : 'application/json' },
+    body: JSON.stringify(updatedNote)
+  }
+
+  const response = await fetch(`${baseUrl}/${updatedNote.id}`, options)
+
+  if(!response.ok) {
+    throw new Error('Failed to update note')
+  }
+
+  return await response.json()
 }
