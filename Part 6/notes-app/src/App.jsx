85a5a44aeeed184af2bb03341e6ffb397a468e54
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-07-13 11:00:18 +0300

    Optimizing the performance

--- Part 6/notes-app/src/App.jsx
+++ Part 6/notes-app/src/App.jsx
@@ -7,8 +7,9 @@ const App = () => {
 
   const newNoteMutation = useMutation({
     mutationFn: createNote,
-    onSuccess: () => {
-      queryClient.invalidateQueries({queryKey: ['notes']})
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData(['notes'])
+      queryClient.setQueryData(['notes'], notes.concat(newNote))
     }
   })
 
@@ -34,7 +35,8 @@ const App = () => {
 
 const result = useQuery({
   queryKey: ['notes'],
-  queryFn: getNotes
+  queryFn: getNotes,
+  refetchOnWindowFocus: false
 })
 
 console.log(JSON.parse(JSON.stringify(result)))
