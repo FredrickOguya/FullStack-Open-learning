a42acf982ea62fb21a8081c3527cbe66271dd54e
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-07-21 11:56:37 +0300

    hooks first commit

---
+++ Part-7/hooks-app/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import { useCounter } from './hooks/useCounter'
+
+function App() {
+  const counter = useCounter()
+  
+
+  return (
+    <div>
+      <div>{counter.value}</div>
+      <button onClick={counter.increase}>
+        plus
+      </button>
+      <button onClick={counter.decrease}>
+        minus
+      </button>
+      <button onClick={counter.zero}>
+        zero
+      </button>
+    </div>
+  )
+}
+
+export default App
