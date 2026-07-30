d35470817c487473fe422519e4c595a0e7788d1f
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-07-30 13:51:01 +0300

    handling mutation error

--- GraphQL/frontend/src/App.jsx
+++ GraphQL/frontend/src/App.jsx
@@ -2,19 +2,30 @@ import { useQuery } from "@apollo/client/react"
 import Persons from "./components/Persons"
 import PersonForm from "./components/PersonForm"
 import { ALL_PERSONS } from "./queries"
+import { useState } from "react"
+import Notify from "./components/Notify"
 
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS)
 
   if(result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    },1000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage}/>
       <Persons persons={result.data.allPersons}/>
-      <PersonForm />
+      <PersonForm setError={notify}/>
     </div>
   )
 }
