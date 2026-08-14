acb7a953d30063018f3bb0f645526c8583553917
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-08-14 07:07:04 +0300

    changing the port and importing useMutation correctly

--- GraphQL/frontend/src/components/LoginForm.jsx
+++ GraphQL/frontend/src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useMutation } from "@apollo/client"
+import { useMutation } from "@apollo/client/react"
 import { LOGIN } from "../queries"
 
 const LoginForm = ({ setError, setToken }) => {
