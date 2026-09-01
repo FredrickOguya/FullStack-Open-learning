4c5150e41ef472503b71d5ef5f79cafa64353f0f
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-09-01 10:20:25 +0300

    Parsing request body in middleware

--- Part 9/Typescript/Ilaris_flight_diaries/src/routes/diaries.ts
+++ Part 9/Typescript/Ilaris_flight_diaries/src/routes/diaries.ts
@@ -1,27 +1,40 @@
-import express, { type Response } from 'express'
+import express, { type NextFunction, type Response, type Request } from 'express'
 import diaryService from '../services/diaryService.ts';
-import type { NonSensitiveDiaryEntry } from '../types.ts';
-import parseNewDiaryentry from '../utils.ts'
+import { NewEntrySchema, type DiaryEntry, type NewDiaryEntry, type NonSensitiveDiaryEntry } from '../types.ts';
+import z from 'zod';
 
 const router = express.Router();
+
+const newDiaryParser = (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    NewEntrySchema.parse(req.body);
+    next();
+  } catch (error: unknown){
+    next(error);
+  }
+}
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => {
+  if(error instanceof z.ZodError){
+    res.status(400).send({ error: error.issues })
+  } else {
+    next(error);
+  }
+}
 
 router.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
   res.send(diaryService.getNonSensitiveEntries())
 });
 
-router.post('/', (req, res) => {
-  try {
-    const newDiaryEntry = parseNewDiaryentry(req.body);
-    const addedEntry = diaryService.addDiary(newDiaryEntry)
-    res.json(addedEntry);
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if(error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    res.status(400).send(errorMessage);
-  }
+
+
+
+
+router.post('/', newDiaryParser, (req: Request<unknown, unknown, NewDiaryEntry>, res: Response<DiaryEntry>) => {  
+  const addedEntry = diaryService.addDiary(req.body);  
+  res.json(addedEntry);
 });
+
 
 router.get('/:id', (req, res) => {
   const diary = diaryService.findById(Number(req.params.id));
@@ -32,5 +45,7 @@ router.get('/:id', (req, res) => {
     res.sendStatus(404)
   }
 })
+
+router.use(errorMiddleware)
 
 export default router;
--- Part 9/Typescript/Ilaris_flight_diaries/src/types.ts
+++ Part 9/Typescript/Ilaris_flight_diaries/src/types.ts
@@ -1,3 +1,5 @@
+import z from "zod";
+
 export const  Weather = {
   Sunny: 'sunny',
   Rainy: 'rainy',
@@ -13,17 +15,23 @@ export const Visibility = {
   Poor: 'poor'
 } as const;
 
-export interface DiaryEntry {
-  id: number;
-  date: string;
-  weather: Weather;
-  visibility: Visibility;
-  comment?: string;
+export const NewEntrySchema = z.object({
+  weather: z.enum(Weather),
+  visibility: z.enum(Visibility),
+  date: z.iso.date(),
+  comment: z.string().optional()
+});
+
+export type NewDiaryEntry = z.infer<typeof NewEntrySchema>;
+
+export interface DiaryEntry extends NewDiaryEntry {
+  id: number
 }
 
 export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
 
-export type NewDiaryEntry = Omit<DiaryEntry, 'id'>;
 
 export type Weather = typeof Weather[keyof typeof Weather];
 export type Visibility = typeof Visibility[keyof typeof Visibility];
+
+
